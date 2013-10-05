d35cd99886ae5d7de82b1e51bb66af086220dc77
trevorlatson <trevor.latson@gmail.com>
2013-10-05 11:18:36 -0500

    fixed bug due to camera app returning null. Use the public Uri fileUri to access the picture saved by the camera

--- src/ginger/connexus/activity/BaseActivity.java
+++ src/ginger/connexus/activity/BaseActivity.java
@@ -24,7 +24,7 @@ public class BaseActivity extends FragmentActivity {
     public static final int MEDIA_TYPE_VIDEO = 2;
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 200;
-    private Uri fileUri;
+    public Uri fileUri;
     
     /** Create a file Uri for saving an image or video */
     private static Uri getOutputMediaFileUri(int type){
@@ -37,14 +37,14 @@ public class BaseActivity extends FragmentActivity {
         // using Environment.getExternalStorageState() before doing this.
 
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                  Environment.DIRECTORY_PICTURES), "MyCameraApp");
+                  Environment.DIRECTORY_PICTURES), "Connexus");
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
 
         // Create the storage directory if it does not exist
         if (! mediaStorageDir.exists()){
             if (! mediaStorageDir.mkdirs()){
-                Log.d("MyCameraApp", "failed to create directory");
+                Log.d("Connexus", "failed to create directory");
                 return null;
             }
         }
@@ -83,7 +83,8 @@ public class BaseActivity extends FragmentActivity {
         case R.id.takepic:
         	// create Intent to take a picture and return control to the calling application
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
+            
+            // fileUri is public. Use this handle to access the saved image from other classes. 
             fileUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE); // create a file to save the image
             intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); // set the image file name
 
@@ -103,28 +104,26 @@ public class BaseActivity extends FragmentActivity {
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Image captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Image saved to:\n" +
-                         data.getData(), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Image saved", Toast.LENGTH_LONG).show();
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the image capture
-            	Toast.makeText(this, "Canceled\n" +
-                        data.getData(), Toast.LENGTH_LONG).show();
+            	Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show();
             } else {
                 // Image capture failed, advise user
-            	Toast.makeText(this, "Image capture failed" +
-                        data.getData(), Toast.LENGTH_LONG).show();
+            	Toast.makeText(this, "Image capture failed", Toast.LENGTH_LONG).show();
             }
         }
 
         if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE) {
             if (resultCode == RESULT_OK) {
                 // Video captured and saved to fileUri specified in the Intent
-                Toast.makeText(this, "Video saved to:\n" +
-                         data.getData(), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Video saved", Toast.LENGTH_LONG).show();
             } else if (resultCode == RESULT_CANCELED) {
                 // User cancelled the video capture
+            	Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show();
             } else {
                 // Video capture failed, advise user
+            	Toast.makeText(this, "Video capture failed", Toast.LENGTH_LONG).show();
             }
         }
     }
